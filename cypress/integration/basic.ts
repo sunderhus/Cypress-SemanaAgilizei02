4527540af6cbe442cbee1b752f206d2d04d53266
Matheus Sunderhus <matheus.sunderhus@interprocess.com.br>
2020-12-11 14:48:36 -0300

    refactore: change some cypress configurations on cypress.json file. -mfeature: Create e2e test of buying a product.

--- cypress/integration/basic.ts
+++ cypress/integration/basic.ts
@@ -1,11 +1,54 @@
-describe("Searchs",()=>{
-    it('Should be able to performe google search', () => {
-        cy.visit('https://www.google.com.br');
-        cy.get('[name="q"]')
-            .type('Olá mundo')
-            .type('{enter}');
-    });
-    it('Should be able to search base site', () => {
+describe('Buy', () => {
+    it('should be able to buy a product', () => {
+        const productName = 'Faded Short Sleeve T-shirts';
+
         cy.visit('/');
+
+        cy.contains(productName)
+            .trigger('mouseover')
+            .parent()
+            .siblings('div.button-container')
+            .children('a')
+            .first()
+            .click();
+
+        cy.get('.button-container')
+            .children('a[href$="controller=order"]')
+            .click();
+
+        cy.get('.cart_navigation')
+            .children(`a[href$='order&step=1']`)
+            .click();
+
+        cy.get('#email')
+            .type('semana-agilizei@mail.com');
+        cy.get('#passwd')
+            .type('12345');
+
+        cy.get('#SubmitLogin')
+            .click();
+
+        //cy.get("#addressesAreEquals");
+
+        cy.get("button[name='processAddress']")
+            .click();
+
+        cy.get(`[type='checkbox']#cgv`).check();
+
+        cy.get("button[name='processCarrier']")
+            .click();
+
+        cy.get(`.bankwire`).click();
+
+        cy.get(`.cart_navigation`)
+            .children(`button[type="submit"]`)
+            .find("span")
+            .contains("I confirm my order")
+            .click();
+
+        cy.get(`.cheque-indent`)
+            .children("strong")
+            .should('contain.text', 'Your order on My Store is complete.');
+
     });
-});
+})
