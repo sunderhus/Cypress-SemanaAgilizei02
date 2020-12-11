49ab1087d9beac8850761f32a26789241413a252
Matheus Sunderhus <matheus.sunderhus@interprocess.com.br>
2020-12-11 10:58:11 -0300

    feature: add license file

--- cypress/integration/basic.ts
+++ cypress/integration/basic.ts
@@ -1,5 +1,11 @@
-describe("Search", () => {
-    it('Should be able to search', () => {
+describe("Searchs",()=>{
+    it('Should be able to performe google search', () => {
+        cy.visit('https://www.google.com.br');
+        cy.get('[name="q"]')
+            .type('Olá mundo')
+            .type('{enter}');
+    });
+    it('Should be able to search base site', () => {
         cy.visit('/');
     });
 });
